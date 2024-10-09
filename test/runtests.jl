27b07c123c83d6c23e091421bc41278b9b4b7863
dhtan <dinghangtan@gmail.com>
2024-10-10 02:27:34 +0800

    init

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using PostTensorBoard
-using Test
 
-@testset "PostTensorBoard.jl" begin
-    # Write your tests here.
-end
+# ---- output pdf ----
+path = "/export/home/tandh/JlProjects/HeatFlowTopOpt/data/2024-09-24T17_52_08"
+tags = ["energy/E" "energy/Jt"]
+key = ["InitType"]
+ex_tags = String[]
+output_with_keys([path], key, tags; ex_tags= ex_tags)
+
+# ---- output mp4 ----
+tb_path = joinpath(path, "tb")
+runs = [1]
+domain2mp4(tb_path, runs)
